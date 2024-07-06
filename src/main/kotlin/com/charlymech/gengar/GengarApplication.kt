b2bc7879981fda1f6dab731414f0482f18a75c68
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-07-06 22:22:20 +0200

    INIT REPO

---
+++ src/main/kotlin/com/charlymech/gengar/GengarApplication.kt
@@ -0,0 +1,11 @@
+package com.charlymech.gengar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GengarApplication
+
+fun main(args: Array<String>) {
+	runApplication<GengarApplication>(*args)
+}
---
+++ src/test/kotlin/com/charlymech/gengar/GengarApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.charlymech.gengar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GengarApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
