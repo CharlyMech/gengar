e421a7c20e5ac4816f09cfdd55884b19bea8810f
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-07-09 16:53:04 +0200

    feat: set companion object for API base url

--- src/main/kotlin/com/charlymech/gengar/App.kt
+++ src/main/kotlin/com/charlymech/gengar/App.kt
@@ -2,25 +2,17 @@ package com.charlymech.gengar
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import io.github.cdimascio.dotenv.dotenv
+
 
 @SpringBootApplication
-class App
-
-fun main(args: Array<String>) {
-	runApplication<App>(*args)
+class App {
+	companion object {
+		val dotenv = dotenv();
+		val BASE_URL: String = dotenv["POKE_API"] ?: "https://pokeapi.co/api/v2/";
+	}
 }
 
-data class Message(val id: String?, val text: String)
-
-@RestController
-class MessageController {
-	@GetMapping("/")
-	fun index() = listOf(
-		Message("1", "Hello!"),
-		Message("2", "Bonjour!"),
-		Message("3", "Privet!"),
-	);
+fun main(args: Array<String>) {
+	runApplication<App>(*args);
 }
